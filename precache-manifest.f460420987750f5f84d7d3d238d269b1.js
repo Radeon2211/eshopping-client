3b2d077508dcd532759ad5d79dd97cf4bf48765b
Radosław Mikrut <radeon.m03@gmail.com>
2020-07-14 13:53:09 +0200

    Updates

---
+++ precache-manifest.f460420987750f5f84d7d3d238d269b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b6be106d14d9d182aeaa7dac291dbaf",
+    "url": "/eshopping-client/index.html"
+  },
+  {
+    "revision": "5c954cd588a39146cc74",
+    "url": "/eshopping-client/static/js/2.21e8fe8e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/eshopping-client/static/js/2.21e8fe8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d21d305906e6ed8fe26b",
+    "url": "/eshopping-client/static/js/main.f10399c1.chunk.js"
+  },
+  {
+    "revision": "5b469d27e5daf61c3f06",
+    "url": "/eshopping-client/static/js/runtime-main.309ec03a.js"
+  }
+]);
