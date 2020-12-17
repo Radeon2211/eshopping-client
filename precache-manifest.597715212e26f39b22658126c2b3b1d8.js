384121c1840501f4bfc7a26855d26937b58e79ce
Radosław Mikrut <radeon.m03@gmail.com>
2020-12-17 21:04:22 +0100

    Updates

---
+++ precache-manifest.597715212e26f39b22658126c2b3b1d8.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ec3f29af95af23dc7913067f00ae832",
+    "url": "/eshopping-client/index.html"
+  },
+  {
+    "revision": "34f7d63aeb269f814e8d",
+    "url": "/eshopping-client/static/js/2.76a9d3ea.chunk.js"
+  },
+  {
+    "revision": "f925ff808c907f1e05af05a547934faf",
+    "url": "/eshopping-client/static/js/2.76a9d3ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca4151bc3beef607dd77",
+    "url": "/eshopping-client/static/js/3.7930e8ce.chunk.js"
+  },
+  {
+    "revision": "1d7a41648a4768442733",
+    "url": "/eshopping-client/static/js/4.0376b8e8.chunk.js"
+  },
+  {
+    "revision": "c08c0186bf4159a7cf45",
+    "url": "/eshopping-client/static/js/5.da59e3e3.chunk.js"
+  },
+  {
+    "revision": "0aa46ef02e27f9fad58b",
+    "url": "/eshopping-client/static/js/main.d1d84d4b.chunk.js"
+  },
+  {
+    "revision": "02b1227c4c5b7d01ba6c",
+    "url": "/eshopping-client/static/js/runtime-main.f9a83a94.js"
+  },
+  {
+    "revision": "db646eb5c58ea1936b92346615a89a11",
+    "url": "/eshopping-client/static/media/add-product.db646eb5.svg"
+  },
+  {
+    "revision": "cd8c1b412e9014ad062c0621afc714ad",
+    "url": "/eshopping-client/static/media/arrow.cd8c1b41.svg"
+  },
+  {
+    "revision": "ff00023590d69e53dcae9cffd1b6ebd3",
+    "url": "/eshopping-client/static/media/cart.ff000235.svg"
+  },
+  {
+    "revision": "ea030947e17cc26f99fbe95ecc08274d",
+    "url": "/eshopping-client/static/media/check-mark.ea030947.svg"
+  },
+  {
+    "revision": "80b9a7c2e284f51e13c6a523127ccb72",
+    "url": "/eshopping-client/static/media/email.80b9a7c2.svg"
+  },
+  {
+    "revision": "cdc3804680fc92a7bfd05556e714e4bd",
+    "url": "/eshopping-client/static/media/filters.cdc38046.svg"
+  },
+  {
+    "revision": "5b238434e2862c877f08572b96c0ef7b",
+    "url": "/eshopping-client/static/media/linkedin.5b238434.svg"
+  },
+  {
+    "revision": "24ab44e25a6d5299076516a86ab57559",
+    "url": "/eshopping-client/static/media/log-out.24ab44e2.svg"
+  },
+  {
+    "revision": "839e1024cb6f8556e0db76bdb7dd1ea3",
+    "url": "/eshopping-client/static/media/minus.839e1024.svg"
+  },
+  {
+    "revision": "82138a406c0927f27e641886e8935a2e",
+    "url": "/eshopping-client/static/media/my-account.82138a40.svg"
+  },
+  {
+    "revision": "4cbeefcf156e5bb35ae44d159044c86a",
+    "url": "/eshopping-client/static/media/my-offers.4cbeefcf.svg"
+  },
+  {
+    "revision": "f908dfcbbd71967c8ea12fcc1b37c3cf",
+    "url": "/eshopping-client/static/media/placed-orders.f908dfcb.svg"
+  },
+  {
+    "revision": "ab8cc7234e9b5879f0c538968c4462d0",
+    "url": "/eshopping-client/static/media/plus.ab8cc723.svg"
+  },
+  {
+    "revision": "4c32f726bb4e6d555c4769d4bd6cdcf8",
+    "url": "/eshopping-client/static/media/search.4c32f726.svg"
+  },
+  {
+    "revision": "40b39a4fd618664010cf75552bd325fa",
+    "url": "/eshopping-client/static/media/sell-history.40b39a4f.svg"
+  },
+  {
+    "revision": "6375717a86bdcd558c522aff052e27d2",
+    "url": "/eshopping-client/static/media/trash.6375717a.svg"
+  }
+]);
