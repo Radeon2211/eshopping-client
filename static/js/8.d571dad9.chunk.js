b0ca1e8b28783b6b48e6a31d7c6d3114fa2a9567
Radosław Mikrut <radeon.m03@gmail.com>
2021-04-10 15:21:51 +0200

    Updates

---
+++ static/js/8.d571dad9.chunk.js
@@ -0,0 +1,1 @@
+(this["webpackJsonpeshopping-app-client"]=this["webpackJsonpeshopping-app-client"]||[]).push([[8],{362:function(t,e,n){"use strict";n.r(e);var c=n(0),p=n(6),s=n(10),i=n(1);e.default=function(t){var e=t.history,n=Object(p.b)(),u=Object(c.useCallback)((function(){return n(s.u())}),[n]);return Object(c.useEffect)((function(){u(),e.goBack()}),[u,e]),Object(i.jsx)(i.Fragment,{})}}}]);
